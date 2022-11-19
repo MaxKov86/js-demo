4aa630495a626fc4c1aa67352f045d07203d446d
MaxKov86 <max.kov86@ukr.net>
2022-11-19 19:24:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,251 @@
-console.log("Hello World");
+// console.log("Hello World!");
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// let password = "";
+
+// do {
+//   password = prompt("Введіть пароль довший 4-х символів", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+// for (let i = 0; i <= 20; i += 5) {
+//   console.log(i);
+// }
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// clients[0] = "Kiwi";
+// clients[2] = "Orange";
+
+// console.log(clients[0]);
+// console.log(clients[1]);
+// console.log(clients[2]);
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// console.log(clients.length);
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.includes("Poly")); // true
+// console.log(clients.includes("Monkong")); // false
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i += 1) { // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for(const key in apartment) {
+//  console.log(key);
+//   console.log(apartment[key]);
+// }
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// book.pageCount = 836;
+// book.originalLanguage = "en";
+// book.translations = ["ua", "ru"];
+
+// console.log(book.pageCount);
+// console.log(book.originalLanguage);
+// console.log(book.translations);
+
+// const bookShelf = {
+//   books: ["The Last Kingdom", "Dream Guardian"],
+//   // Це метод об'єкта
+//   getBooks() {
+//     console.log("Цей метод буде повертати всі книги - властивість books");
+//   },
+//   // Це метод об'єкта
+//   addBook(bookName) {
+//     console.log("Цей метод буде додавати нову книгу у властивість books");
+//   },
+// };
+
+// // Виклики методів
+// bookShelf.getBooks();
+// bookShelf.addBook("Нова книга");
+
+// console.log(bookShelf);
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(book[key]);
+// }
+
+// const lastWeekTemps = [14, 25, 11];
+// const currentWeekTemps = [23, 17, 18];
+// const allTemps = [...currentWeekTemps, ...lastWeekTemps];
+// console.log(allTemps); // [14, 25, 11, 23, 17, 18]
+
+// function multiply(...args) {
+//   console.log(args);
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значення першого аргументу
+//   console.log(secondNumber); // Значення другого аргументу
+//   console.log(otherArgs); // Масив інших аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   stats: { followers, views: userViews, likes: userLikes },
+// } = user;
+
+// console.log(name); // Jacques Gluke
+// console.log(tag); // jgluke
+// console.log(followers); // 5603
+// console.log(userViews); // 4827
+// console.log(userLikes);
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// const accessType = book.isPublic ? "публічному" : "закритому";
+// const message = `Книга ${book.title} автора ${book.author} з рейтингом ${book.rating} знаходиться в ${accessType} доступі.`;
+
+// // const products = [
+// //   { name: "Radar", price: 1300, quantity: 4 },
+// //   { name: "Scanner", price: 2700, quantity: 3 },
+// //   { name: "Droid", price: 400, quantity: 7 },
+// //   { name: "Grip", price: 1200, quantity: 9 },
+// // ];
+
+// // console.table(products)
+
+// // function getProductPrice(productName) {
+
+// }
+
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// // Передаємо інлайн функцію greet у якості колбека
+// registerGuest("Манго", function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// });
+
+// // Передаємо інлайн функцію notify у якості колбека
+// registerGuest("Полі", function notify(name) {
+//   console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+// });
+
+// function processCall(recipient) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     console.log(`Абонент ${recipient} недоступний, залиште повідомлення.`);
+//     // Логіка активації автовідповідача
+//   } else {
+//     console.log(`З'єднуємо з ${recipient}, очікуйте...`);
+//     // Логіка прийняття дзвінка
+//   }
+// }
+
+// processCall("Mango");
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+// orderedItems.forEach(calculateTotalPrice(element){
+//   totalPrice += element;
+// })
+  
+//   // Change code above this line
+//   return totalPrice;
+// }
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(item => {
+//    return totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+// console.log(calculateTotalPrice)
